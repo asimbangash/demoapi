7a20b288728e973751d212119cab90e78f316f37
asimbangash <asimbangash1999@gamil.com>
2022-11-06 21:54:22 +0500

    cors added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./routers/men");
 require("./db/conn");
 const MenRanking  = require("./models/mens");
 const app = express();
 
+app.use(cors());
 const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(router);
